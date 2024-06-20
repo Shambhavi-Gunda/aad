6173333ca3d8ea7862a2156b3bf42453dcfe31b1
Shambhavi <shambhavi.gunda@gmail.com>
2024-06-20 17:59:51 +0530

    create and launch another activity

---
+++ app/src/main/java/com/example/shambhavi/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.example.shambhavi
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var data = intent.extras?.getString("mykey" )
+        Log.i("homeactivity",data.toString())
+        var homeTextView: TextView = findViewById(R.id.tvHome)
+        homeTextView.setText(data)
+
+    }
+}
