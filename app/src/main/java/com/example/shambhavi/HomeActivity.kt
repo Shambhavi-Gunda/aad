46c8a7e7ab9aabdab05fd2c45a7f6756e5251a8f
Shambhavi <shambhavi.gunda@gmail.com>
2024-07-05 16:53:52 +0530

    HomeActivity launcher

--- app/src/main/java/com/example/shambhavi/HomeActivity.kt
+++ app/src/main/java/com/example/shambhavi/HomeActivity.kt
@@ -1,27 +1,30 @@
 package com.example.shambhavi
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        var data = intent.extras?.getString("mykey" )
+        //get intent which sttarted this activity
+        //get the extras from that intent
+        //get the string with mykey
+        var data = intent.extras?.getString("mykey")
         Log.i("homeactivity",data.toString())
-        var homeTextView: TextView = findViewById(R.id.tvHome)
+        //put the data either in a log or on the textview
+        var homeTextView:TextView = findViewById(R.id.tvHome)
         homeTextView.setText(data)
 
+        */
     }
 }
